f91d4e5b7472f620ad44020b27d86032d9f8ee4a
Alana Saavedra <alasaave@gmail.com>
2018-02-04 21:18:55 -0600

    Add presidents branch and info

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 var iceCream = ['mint', 'caramel', 'coffee',
     'peanut butter'];
+
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: '4 years', party: 'Republican', yearsOfPresidency: 1},
+    {firstName: 'Barack', lastName: 'Obama', termLength: '4 years', party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George W.', lastName: 'Bush', termLength: '4 years', party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: '4 years', party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush', termLength: '4 years', party: 'Republican', yearsOfPresidency: 4},
+];
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
